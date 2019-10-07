2753710518deca69a84718d1b9d4c169eccb51b6
Panna Kristóf <kristofpanna@gmail.com>
2019-10-07 19:36:21 +0200

    Add classes with method stubs

---
+++ src/main/java/com/codecool/kristofpanna/FilePartReader.java
@@ -0,0 +1,42 @@
+package com.codecool.kristofpanna;
+
+public class FilePartReader {
+    String filePath;
+    Integer fromLine;
+    Integer toLine;
+
+    public FilePartReader() {
+        /*
+        sets the class' instance variables to some invalid default value
+         */
+        // TODO
+    }
+
+    public void setup(String filePath, Integer fromLine, Integer toLine) throws IllegalArgumentException {
+        /*
+        it throws an IllegalArgumentException :
+            if toLine is smaller than fromLine
+            if fromLine is smaller than 1
+         */
+        // TODO
+    }
+
+    public String read() {
+        /*
+        opens the file on filePath , and gives back it's content as a String
+         */
+        // TODO
+        return null;
+    }
+
+    public String readLines() {
+        /*
+        - reads the file with read ()
+        - it gives back every line from it's content between fromLine and toLine (both of them are included), and returns these lines as a String.
+            Take care because if fromLine is 1, it means the very first row in the file. Also, if fromLine is 1 and toLine is 1 also, we will read only the very first line.
+         */
+        // TODO
+        return null;
+    }
+
+}
---
+++ src/main/java/com/codecool/kristofpanna/FileWordAnalyzer.java
@@ -0,0 +1,39 @@
+package com.codecool.kristofpanna;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileWordAnalyzer {
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+    }
+
+    public List getWordsOrderedAlphabetically() {
+        /*
+        calls FilePartReader.readLines ()
+        returns the words ordered by alphabetically as an ArrayList
+         */
+        // TODO
+        List wordsOrderedAlphabetically = new ArrayList();
+        return wordsOrderedAlphabetically;
+    }
+
+    public List getWordsContainingSubstring(String subString) {
+        /*
+        calls FilePartReader.readLines ()
+        returns the words which contains the subString
+         */
+        // TODO
+        return null;
+    }
+
+    public List getStringsWhichPalindromes() {
+        /*
+        calls FilePartReader.readLines ()
+        returns the words from the String which are palindrome
+         */
+        // TODO
+        return null;
+    }
+
+
+}
