882d315df3241761e7ae4b09c2ddb413d1fb727d
Panna Kristóf <kristofpanna@gmail.com>
2019-10-09 16:16:27 +0200

    Test and impl. readLines(); fix condition and tests for setup (arg order)

--- src/main/java/com/codecool/kristofpanna/FilePartReader.java
+++ src/main/java/com/codecool/kristofpanna/FilePartReader.java
@@ -15,13 +15,20 @@ public class FilePartReader {
          */
     }
 
+    /**
+     * Set up the reader parameters.
+     * @param filePath file to read
+     * @param fromLine index of first line to read (from 1)
+     * @param toLine index of last line to read (from 1)
+     * @throws IllegalArgumentException if toLine < fromLine, or fromLine < 1
+     */
     public void setup(String filePath, Integer fromLine, Integer toLine) throws IllegalArgumentException {
         /*
         it throws an IllegalArgumentException :
             if toLine is smaller than fromLine
             if fromLine is smaller than 1
          */
-        if (toLine > fromLine) {
+        if (toLine < fromLine) {
             throw new IllegalArgumentException("toLine is smaller than fromLine.");
         }
         if (fromLine < 1) {
@@ -46,8 +53,26 @@ public class FilePartReader {
         - it gives back every line from it's content between fromLine and toLine (both of them are included), and returns these lines as a String.
             Take care because if fromLine is 1, it means the very first row in the file. Also, if fromLine is 1 and toLine is 1 also, we will read only the very first line.
          */
-        // TODO
-        return null;
+        String whole;
+        try {
+            whole = read();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        String[] wholeLines = whole.split("(?<=\n)");  // linebreak char stays at the end of lines
+
+        int wholeLength = wholeLines.length;
+        if (wholeLength < fromLine) {
+            return "";
+        }
+        int endLine = Math.min(toLine, wholeLength);
+        final int length = endLine - fromLine + 1;
+
+        String[] partLines = new String[length];
+        System.arraycopy(wholeLines, fromLine - 1, partLines, 0, length);
+
+        return String.join("", partLines);
     }
 
 }
--- src/test/java/com/codecool/kristofpanna/FilePartReaderTest.java
+++ src/test/java/com/codecool/kristofpanna/FilePartReaderTest.java
@@ -9,8 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FilePartReaderTest {
-    private FilePartReader filePartReader;
     private static final String validFilePath = "src/test/resources/file_to_read1.txt";
+    private static final String invalidFilePath = "src/test/resources/non_existent_file.txt";
+
+    private FilePartReader filePartReader;
 
     @BeforeEach
     void setUp() {
@@ -25,22 +27,22 @@ public class FilePartReaderTest {
     }
 
     @Test
-    void setup_toLineIsSmallerThanFromLine_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, ()-> {
-            filePartReader.setup(validFilePath, 1, 2);
+    void setup_toLineIsSmallerThanFromLine_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            filePartReader.setup(validFilePath, 2, 1);
         });
     }
 
     @Test
-    void setup_fromLineIsSmallerThan1_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, ()-> {
-            filePartReader.setup(validFilePath, 0, -1);
+    void setup_fromLineIsSmallerThan1_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            filePartReader.setup(validFilePath, 0, 1);
         });
     }
 
     @Test
     void setup_validValues_valuesNotNull() {
-        filePartReader.setup(validFilePath, 2, 1);
+        filePartReader.setup(validFilePath, 1, 2);
         assertNotNull(filePartReader.filePath, "filePath is null after calling setup");
         assertNotNull(filePartReader.fromLine, "fromLine is null after calling setup");
         assertNotNull(filePartReader.toLine, "toLine is null after calling setup");
@@ -48,7 +50,7 @@ public class FilePartReaderTest {
 
     @Test
     void read_validFilePath_returnsContent() throws IOException {
-        filePartReader.setup(validFilePath, 2, 1);
+        filePartReader.setup(validFilePath, 1, 2);
         assertEquals(filePartReader.read(),
                 "The monkey 123 is the only monkey here.\n" +
                         "No others.\n" +
@@ -57,9 +59,43 @@ public class FilePartReaderTest {
 
     @Test
     void read_invalidFilePath_throwsIOException() {
-        filePartReader.setup("src/test/resources/non_existent_file.txt", 2, 1);
+        filePartReader.setup(invalidFilePath, 1, 2);
         assertThrows(IOException.class, () -> {
             filePartReader.read();
         });
+    }
+
+    @Test
+    void readLines_firstLineToFirstLine_returnsFirstLine() {
+        filePartReader.setup(validFilePath, 1, 1);
+        assertEquals(filePartReader.readLines(), "The monkey 123 is the only monkey here.\n"); // with the linebreak char at the end
+    }
+
+    @Test
+    void readLines_multipleLines_returnsMultipleLines() {
+        filePartReader.setup(validFilePath, 2, 3);
+        assertEquals(filePartReader.readLines(),
+                "No others.\n" +
+                        "Just the one.\n");
+    }
+
+    @Test
+    void readLines_tooBigFromValue_returnsEmpty() {
+        filePartReader.setup(validFilePath, 5, 8);
+        assertEquals(filePartReader.readLines(), "");
+    }
+
+    @Test
+    void readLines_tooBigToValue_returnsToEnd() {
+        filePartReader.setup(validFilePath, 2, 4);
+        assertEquals(filePartReader.readLines(),
+                "No others.\n" +
+                        "Just the one.\n");
+    }
+
+    @Test
+    void readLines_ExceptionWhileRead_returnsNull() {
+        filePartReader.setup(invalidFilePath, 1, 2);
+        assertNull(filePartReader.readLines());
     }
 }
